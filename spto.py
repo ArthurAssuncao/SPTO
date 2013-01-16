6a1b64330a9b9581fad64f00d599c097730be6d9
Administrador <administrador@OptiPlex-790.(none)>
2013-01-16 21:19:20 -0200

    Primeiro commit

---
+++ spto.py
@@ -0,0 +1,35 @@
+#coding: utf-8
+
+import re
+import threading
+import time
+import urllib2
+
+def exibe_lista(url='http://www.imdb.com/find?q=O+Hobbit'):
+    html = urllib2.urlopen(url).read()
+    itens = re.findall(r'<td class="result_text"> <a href="(.+?)".+?>(.+?)</td>', html)
+    if len(itens) > 0:
+        for link, item in itens:
+            item = re.sub('<small>.+?</small>', '',item) # Remove tag small
+            item = re.sub('<[^>]*>', '', item) # Remove tags e seus conteúdos
+            if not re.match(r'/name/nm.+?', link): # Somente Titles
+                print 'TÍTULO: {0}\nLINK: {1}\n'.format(item, link)
+    else:
+        print 'Nenhum resultado encontrado'
+
+def gera_url(pesquisa):
+    pesquisa = pesquisa.strip()
+    pesquisa = pesquisa.replace(' ', '+')
+    if len(pesquisa) > 0:
+        return 'http://www.imdb.com/find?q={0}'.format(pesquisa)
+    else:
+        return None
+
+if __name__ == "__main__":
+    pesquisa = raw_input('Buscar: ')
+    url = gera_url(pesquisa)
+    if url:
+        exibe_lista(url)
+    else:
+        print 'Campo de Busca vazio'
+    
